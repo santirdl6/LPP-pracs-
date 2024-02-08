47f698dd10ca66ba750f9971ab049ebb7c92fe4e
santirdl6 <158267373+santirdl6@users.noreply.github.com>
2024-02-08 14:44:07 +0100

    Update practica2.rkt

--- practica2.rkt
+++ practica2.rkt
@@ -11,105 +11,101 @@
   (* x x))
 (define (cubo x)
   (* x x x))
+(define (convierte-digito num posicion)
+  (* num (expt 2 posicion)))
 
 (define (binario-a-decimal b3 b2 b1 b0)
-  (+ (* b3 (cubo 2)) (* b2 (cuadrado 2)) (* b1 2) b0))
+  (+ (convierte-digito b3 3) (convierte-digito b2 2) (convierte-digito b1 1) b0))
 
-(binario-a-decimal 1 1 1 1) ; ⇒ 15
-(binario-a-decimal 0 1 1 0) ; ⇒ 6
-(binario-a-decimal 0 0 1 0) ; ⇒ 2
+
+(check-equal? (binario-a-decimal 1 1 1 1) 15)
+(check-equal? (binario-a-decimal 0 1 1 0) 6)
+(check-equal? (binario-a-decimal 0 0 1 0) 2)
 
 ;b)
 
 (define (numero-a-hexadecimal n)
   (if (> n 9)
   (integer->char (+ (char->integer #\F) (- n 15))) ; Cogemos el máximo valor posible y le quitamos la diferencia
-  n))
+  (integer->char (+ n (char->integer #\0)))))
 
 (define (binario-a-hexadecimal b3 b2 b1 b0)
  (numero-a-hexadecimal (binario-a-decimal b3 b2 b1 b0)))
 
-(binario-a-hexadecimal 1 1 1 1) ; ⇒ #\F
-(binario-a-hexadecimal 0 1 1 0) ; ⇒ #\6
-(binario-a-hexadecimal 1 0 1 0) ; ⇒ #\A
+
+(check-equal? (binario-a-hexadecimal 1 1 1 1) #\F)
+(check-equal? (binario-a-hexadecimal 0 1 1 0) #\6)
+(check-equal? (binario-a-hexadecimal 1 0 1 0) #\A)
 
 ;---------------------------------------------------
 ; Eejercicio 2
 
 
 (define (encuentra-indice char)
-  (if (char-upper-case? char)
-      (- (char->integer char) (char->integer #\A))
-      (- (char->integer char) (char->integer #\a))))
+      (- (char->integer char) (char->integer #\a)))
 
-(encuentra-indice #\a) ; ⇒ 0
-(encuentra-indice #\b) ; ⇒ 1
-(encuentra-indice #\m) ; ⇒ 12
-(encuentra-indice #\z) ; ⇒ 25
 
+(check-equal? (encuentra-indice #\a) 0)
+(check-equal? (encuentra-indice #\b) 1)
+(check-equal? (encuentra-indice #\m) 12)
+(check-equal? (encuentra-indice #\z) 25)
 
 
 (define (encuentra-caracter n)
   (integer->char(+ n (char->integer #\a))))
 
-(encuentra-caracter 0) ; ⇒ #\a
-(encuentra-caracter 1) ; ⇒ #\b
-(encuentra-caracter 12) ; ⇒ #\m
-(encuentra-caracter 25) ; ⇒ #\z
+
+(check-equal? (encuentra-caracter 0) #\a)
+(check-equal? (encuentra-caracter 1) #\b)
+(check-equal? (encuentra-caracter 12) #\m)
+(check-equal? (encuentra-caracter 25) #\z)
 
 
 (define (entre-az? char)
-  (and (or (char-upper-case? char) (char-lower-case? char))
-       (not (equal? char #\ñ))
-       (not (equal? char #\Ñ))))
+  (and (>= (char->integer char) (char->integer #\a)) (<= (char->integer char) (char->integer #\z))))
 
-(entre-az? #\a) ; ⇒ #t
-(entre-az? #\m) ; ⇒ #t
-(entre-az? #\z) ; ⇒ #t
-(entre-az? #\`) ; ⇒ #f
-(entre-az? #\{) ; ⇒ #f
+
+(check-equal? (entre-az? #\ó) #f)
+(check-equal? (entre-az? #\m) #t)
+(check-equal? (entre-az? #\z) #t)
+(check-equal? (entre-az? #\´) #f)
+(check-equal? (entre-az? #\{) #f)
 
 
 (define (rota-indice indice desplazamiento)
-  (cond
-    ((> (+ indice desplazamiento) 25)
-        (- (+ indice desplazamiento) 26))
-    ((< (+ indice desplazamiento) 0)
-        (+ 26 (+ indice desplazamiento)))
-    (else (+ indice desplazamiento))))
+  (modulo (+ indice desplazamiento) 26))
 
 
-(rota-indice 4 24) ; ⇒ 2)
-(rota-indice 4 -5) ; ⇒ 25)
-
-
+(check-equal? (rota-indice 4 24) 2)
+(check-equal? (rota-indice 4 -5) 25)
 
 (define (cifra-caracter char desplazamiento)
-  (if (entre-az? char)
+  (if (entre-az? (char-downcase char))
       (if (char-upper-case? char)
-          (char-upcase  (encuentra-caracter (rota-indice (encuentra-indice char) desplazamiento)))
+          (char-upcase  (encuentra-caracter (rota-indice (encuentra-indice (char-downcase char)) desplazamiento)))
           (encuentra-caracter (rota-indice (encuentra-indice char) desplazamiento)))
   char))
 
 (define (descifra-caracter char desplazamiento)
   (cifra-caracter char (* desplazamiento -1)))
 
-(cifra-caracter #\c 5) ; ⇒ #\h)
-(cifra-caracter #\z -1) ; ⇒ #\y)
-(cifra-caracter #\j 40) ; ⇒ #\x)
-(cifra-caracter #\D 3) ; ⇒ #\G)
-(cifra-caracter #\ñ 3) ; ⇒ #\ñ)
 
-(check-equal? (cifra-caracter #\a -1) #\z);
-(check-equal? (cifra-caracter #\n 10) #\x);
-(check-equal? (cifra-caracter #\n 13) #\a);
+(check-equal? (cifra-caracter #\c 5) #\h)
+(check-equal? (cifra-caracter #\z -1) #\y)
+(check-equal? (cifra-caracter #\j 40) #\x)
+(check-equal? (cifra-caracter #\D 3) #\G)
+(check-equal? (cifra-caracter #\ñ 3) #\ñ)
+(check-equal? (cifra-caracter #\a -1) #\z)
+(check-equal? (cifra-caracter #\a -1) #\z)
+(check-equal? (cifra-caracter #\n 10) #\x)
+(check-equal? (cifra-caracter #\n 13) #\a)
 
 
-(descifra-caracter #\d 3) ; ⇒ #\a)
-(descifra-caracter #\y -1) ; ⇒ #\z)
-(descifra-caracter #\x 40) ; ⇒ #\j)
-(descifra-caracter #\G 3) ; ⇒ #\D)
-(descifra-caracter #\tab 3) ; ⇒ #\tab)
+(check-equal? (descifra-caracter #\d 3) #\a)
+(check-equal? (descifra-caracter #\y -1) #\z)
+(check-equal? (descifra-caracter #\x 40) #\j)
+(check-equal? (descifra-caracter #\G 3) #\D)
+(check-equal? (descifra-caracter #\tab 3) #\tab)
 
 
 ;--------------------------------------------------------
@@ -124,8 +120,11 @@
           n2
           n3)))
 
-(menor-de-tres 2 8 1) ;; ⇒ 1
-(menor-de-tres 3 0 3) ;; ⇒ 0
+
+(check-equal? (menor-de-tres 2 8 1) 1)
+(check-equal? (menor-de-tres 3 0 3) 0)
+(check-equal? (menor-de-tres 2 8 -1) -1)
+
 
 ;version 2 función auxiliar
 (define (menor x y)
@@ -135,9 +134,11 @@
 (define (menor-de-tres-v2 n1 n2 n3)
   (menor (menor n1 n2) n3))
 
-(menor-de-tres-v2 2 8 1) ;; ⇒ 1
-(menor-de-tres-v2 3 0 3) ;; ⇒ 0
-(menor-de-tres-v2 20 30 5) ;; ⇒ 5
+
+(check-equal? (menor-de-tres-v2 2 8 1) 1)
+(check-equal? (menor-de-tres-v2 3 0 3) 0)
+(check-equal? (menor-de-tres-v2 2 8 -1) -1)
+
 
 
 ;------------------------------------
@@ -198,12 +199,13 @@
       (first lista2))))
 
 (define (cadenas-mayores lista1 lista2)
-  (list (cadena-mayor-primer-elemento lista1 lista2)
+  (list(cadena-mayor-primer-elemento lista1 lista2)
         (cadena-mayor-primer-elemento (rest lista1) (rest lista2))
         (cadena-mayor-primer-elemento (rest (rest lista1)) (rest (rest lista2)))))
 
-(cadenas-mayores '("hola" "que" "tal") '("meme" "y" "adios")) ; ⇒ ("hola" "que" "adios")
-(cadenas-mayores '("esto" "es" "lpp") '("hoy" "hay" "clase")) ; ⇒ ("esto" "hay" "clase")
+
+(check-equal? (cadenas-mayores '("hola" "que" "tal") '("meme" "y" "adios")) '("hola" "que" "adios"))
+(check-equal? (cadenas-mayores '("esto" "es" "lpp") '("hoy" "hay" "clase")) '("esto" "hay" "clase"))
 
 
 
@@ -219,9 +221,9 @@
     ((equal? char #\♦) 'Diamantes)
     (else 'Tréboles)))
 
-(obten-palo #\♠) ; ⇒ Picas
-(obten-palo #\♥) ; ⇒ Corazones
-(obten-palo #\♣) ; ⇒ Tréboles
+(check-equal? (obten-palo #\♠) 'Picas)
+(check-equal? (obten-palo #\♥) 'Corazones)
+(check-equal? (obten-palo #\♣) 'Tréboles)
 
 (define (valores-especiales char)
   (cond
@@ -235,11 +237,11 @@
       (- (char->integer char) (char->integer #\0))
       (valores-especiales char)))
 
-(obten-valor #\3) ; ⇒ 3
-(obten-valor #\J) ; ⇒ 10
-(obten-valor #\5) ; ⇒ 3
-(obten-valor #\K) ; ⇒ 10
 
+(check-equal? (obten-valor #\3) '3)
+(check-equal? (obten-valor #\J) '10)
+(check-equal? (obten-valor #\5) '5)
+(check-equal? (obten-valor #\K) '12)
 
 ;Función final
 (define (carta simbolo)
@@ -251,26 +253,30 @@
 (define jota-de-diamantes 'J♦)
 
 ;Comprobaciones
-(carta tres-de-picas) ; ⇒ (3 . Picas)
-(carta as-de-corazones) ; ⇒ (1 . Corazones)
-(carta 'K♣) ; ⇒ (12 . Tréboles)
 
-
+(check-equal? (carta tres-de-picas) '(3 . Picas))
+(check-equal? (carta as-de-corazones) '(1 . Corazones))
+(check-equal? (carta 'K♣) '(12 . Tréboles))
 ;Ejercicio 7
 (define (primer-objeto-carta carta1)
   (car (carta carta1)))
 
+(define (es-pareja n)
+  (string-append "pareja de " (number->string n)))
+
 (define (jugada-mano-simple n1 n2 n3)
   (if (= n1 n2 n3)
       (string-append "trío de " (number->string n1))
-      (cond ((= n1 n2) (string-append "pareja de " (number->string n2)))
-            ((= n1 n3) (string-append "pareja de " (number->string n3)))
-               ((= n2 n3) (string-append "pareja de " (number->string n3)))
+      (cond ((= n1 n2) (es-pareja n1))
+            ((= n1 n3) (es-pareja n3))
+               ((= n2 n3) (es-pareja n2))
                (else "nada"))))
 
 (define (jugada-mano carta1 carta2 carta3)
   (jugada-mano-simple (primer-objeto-carta carta1) (primer-objeto-carta carta2) (primer-objeto-carta carta3)))
 
-(jugada-mano '3♥ '3♣ '3♥) ; ⇒ "trío de 3"
-(jugada-mano 'K♦ '7♠ 'K♥) ; ⇒ "pareja de 12"
-(jugada-mano '5♣ '4♣ '6♣) ; ⇒ "nada"
+
+(check-equal? (jugada-mano '3♥ '3♣ '3♥) "trío de 3")
+(check-equal? (jugada-mano 'K♦ '7♠ 'K♥) "pareja de 12")
+(check-equal? (jugada-mano '5♣ '4♣ '6♣) "nada")
+
